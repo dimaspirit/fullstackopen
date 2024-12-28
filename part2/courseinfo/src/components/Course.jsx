3203ceab097c7dac4ea459159454606b17054254
Dmytro Lobanov <dspwww@gmail.com>
2024-12-28 22:40:11 +0000

    2.2 task DONE

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,11 +1,13 @@
 import Header from "./Header/Header";
 import Content from "./Content/Content";
+import Total from "./Total/Total";
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
