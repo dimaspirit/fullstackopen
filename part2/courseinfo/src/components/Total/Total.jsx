8643d0de9db74ec9b1c0276e0fa5a015072e431a
Dmytro Lobanov <dspwww@gmail.com>
2024-12-28 22:44:23 +0000

    2.3 task DONE

--- part2/courseinfo/src/components/Total/Total.jsx
+++ part2/courseinfo/src/components/Total/Total.jsx
@@ -1,8 +1,10 @@
-const Total = (props) => {
+const Total = ({parts}) => {
+  const total = parts.reduce((accum, part) => accum + part.exercises, 0);
+
   return (
     <p>
       <b>
-        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        Number of exercises: {total}
       </b>
     </p>
   )
