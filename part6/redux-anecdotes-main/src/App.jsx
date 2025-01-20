df715a2a6a7250c778aa36ae853a4341a4ba1f2d
Dmytro Lobanov <dspwww@gmail.com>
2025-01-20 16:25:41 +0000

    6.3 - 6.4 tasks DONE

---
+++ part6/redux-anecdotes-main/src/App.jsx
@@ -0,0 +1,43 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { vote, create } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const handleVote = (id) => {
+    console.log('vote', id)
+    dispatch(vote(id))
+  }
+
+  const handleCreateAnecdote = (e) => {
+    e.preventDefault()
+    const anecdote = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(create(anecdote))
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={handleCreateAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
