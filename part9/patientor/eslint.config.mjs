acdaaaf1cd97f01fb0802864f8d87e5668504bdc
Dmytro Lobanov <dspwww@gmail.com>
2025-02-20 13:04:16 +0000

    9.9 task DONE

--- part9/patientor/eslint.config.mjs
+++ part9/patientor/eslint.config.mjs
@@ -17,7 +17,7 @@ export default tseslint.config({
   plugins: {
     "@stylistic": stylistic,
   },
-  ignores: ["build/*"],
+  ignores: ["build/"],
   rules: {
     '@stylistic/semi': 'error',
     '@typescript-eslint/no-unsafe-assignment': 'error',
