2e17882f4f7d571cd288ed52fc74e3c4ba660360
Dmytro Lobanov <dspwww@gmail.com>
2025-02-27 15:33:22 +0000

    9.18 task DONE

--- part9/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary-frontend/src/App.tsx
@@ -1,16 +1,42 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, SyntheticEvent } from 'react';
 
-interface Diary {
-  id: number;
+interface DiaryBase {
   date: string;
   visibility: string;
   weather: string;
+}
+
+interface DiaryNew extends DiaryBase {
   comment: string;
+}
+
+interface Diary extends DiaryNew {
+  id: number;
 }
 
 function App() {
   const [diaries, setDiares] = useState<Diary[]>([]);
+
+  const [newDiaryItem, setNewDiaryItem] = useState<DiaryNew>({
+    date: '',
+    visibility: '',
+    weather: '',
+    comment: ''
+  })
+
+  const handleSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    fetch('http://localhost:3000/api/diaries', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newDiaryItem)
+    }).then(response => response.json())
+      .then(data => setDiares([...diaries, data]));
+  }
 
   useEffect(() => {
     fetch('http://localhost:3000/api/diaries')
@@ -20,16 +46,53 @@ function App() {
 
   return (
     <>
-      <h1>Flight diary</h1>
+      <h1>Flight diary service</h1>
 
-      {diaries.map((diary) => (
-        <div key={diary.id}>
-          <h2>{diary.date}</h2>
-          <p>visibilty: {diary.visibility}</p>
-          <p>weather: {diary.weather}</p>
-          <p>comment: {diary.comment}</p>
-        </div>
-      ))}
+      <div>
+        <h3>Add new item in diary</h3>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <p>Date</p>
+            <input
+              type="text" name="date" id="date"
+              onChange={(event) => setNewDiaryItem({...newDiaryItem, date: event.target.value})} />
+          </div>
+
+          <div>
+            <p>Visibility</p>
+            <input 
+              type="text" name="visibility" id="visibility"
+              onChange={(event) => setNewDiaryItem({...newDiaryItem, visibility: event.target.value})} />
+          </div>
+
+          <div>
+            <p>Weather</p>
+            <input 
+              type="text" name="weather" id="weather"
+              onChange={(event) => setNewDiaryItem({...newDiaryItem, weather: event.target.value})} />
+          </div>
+
+          <div>
+            <p>Comment</p>
+            <input type="text" name="comment" id="comment" />
+          </div>
+
+          <button type="submit">Add</button>
+        </form>
+      </div>
+
+      <div>
+        <h3>List</h3>
+
+        {diaries.map((diary) => (
+          <div key={diary.id}>
+            <h3>{diary.date}</h3>
+            <p>visibilty: {diary.visibility}</p>
+            <p>weather: {diary.weather}</p>
+            <p>comment: {diary.comment}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
